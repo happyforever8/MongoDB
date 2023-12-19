817e79bc076d238b9eabbc8ec89c05a1b2ae47ce
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2023-12-18 22:17:47 -0500

    Create 店面+onsite.java

---
+++ 店面+onsite.java
@@ -0,0 +1,6 @@
+https://www.1point3acres.com/bbs/thread-709557-1-1.html
+
+电面 手动实现 linkedHashMap，用了hashmap + double linked list
+onsite code 1： 实现含有snapId 的 数据结构, 要求实现以下功能 insert, delete
+onsite code 2：实习爬虫 - 主要是爬取网页中的contact，用了linkedList, 面试官跟进要求实现在短时间内爬取更多contact，经提示改用 priorityQueue，把contact多的页面往前排
+系统设计： 设计聊天系统 whatsup
